7df68fae18efa4a210b1289fd7ee3906b3cd913c
nohawaheed <nohawaheed.1991@gmail.com>
2025-01-18 15:05:19 +0200

    add project routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,58 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./core/pages/login/login.component').then(
+        (c) => c.LoginComponent
+      ),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./core/pages/register/register.component').then(
+        (c) => c.RegisterComponent
+      ),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./core/layout/layout.component').then((c) => c.LayoutComponent),
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./features/pages/home/home.component').then(
+            (c) => c.HomeComponent
+          ),
+      },
+      {
+        path: 'all-category',
+        loadComponent: () =>
+          import('./features/pages/all-category/all-category.component').then(
+            (c) => c.AllCategoryComponent
+          ),
+      },
+      {
+        path: 'about',
+        loadComponent: () =>
+          import('./features/pages/about/about.component').then(
+            (c) => c.AboutComponent
+          ),
+      },
+      {
+        path: 'contact',
+        loadComponent: () =>
+          import('./features/pages/contact/contact.component').then(
+            (c) => c.ContactComponent
+          ),
+      },
+    ],
+  },
+];
