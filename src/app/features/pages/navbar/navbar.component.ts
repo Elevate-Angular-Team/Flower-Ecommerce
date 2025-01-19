e3928345b30cb298f48723bc70312dea5c461cb8
YoussefMohamed <ym0998682@gmail.comm>
2025-01-19 22:55:53 +0200

    add navbar component and integrate Flowbite service; update layout and styles icon the page and ul ,li

--- src/app/features/pages/navbar/navbar.component.ts
+++ src/app/features/pages/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FlowbiteService } from '../../../core/services/flowbite/flowbite.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(private flowbiteService: FlowbiteService) {}
 
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite(flowbite => {
+      // Your custom code here
+      console.log('Flowbite loaded', flowbite);
+    });
+  }
 }
