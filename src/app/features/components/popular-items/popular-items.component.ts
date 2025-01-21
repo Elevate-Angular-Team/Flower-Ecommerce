5a824ef0de20a04659565e4c08ed75ef10eb91bb
muhammed <muhammedabdelftah11@gmail.com>
2025-01-21 02:09:23 +0200

    signals version

--- src/app/features/components/popular-items/popular-items.component.ts
+++ src/app/features/components/popular-items/popular-items.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { MainServicesService } from '../../../core/services/main-services.service';
 import { CommonModule } from '@angular/common';
 import { PopularItems } from '../../../core/interfaces/PopularItems';
@@ -11,38 +11,26 @@ import { Category } from '../../../core/interfaces/category';
   styleUrl: './popular-items.component.scss'
 })
 export class PopularItemsComponent implements OnInit {
+  // Limit for the number of categories to fetch
+  limit: number = 4;
+
+  // Signals for state management
+  categories = signal<Category[]>([]);
+  products = signal<PopularItems[]>([]);
+  selectedCategoryIndex = signal<number>(0);
+
+  // Injecting the main service
   constructor(private mainService: MainServicesService) {}
 
-  limit: number = 4;
-  categories: Category[] = [];
-  products: PopularItems[] = [];
-  selectedCategoryIndex: number = 0; 
-
+  // Lifecycle hook that is called after data-bound properties are initialized
   ngOnInit(): void {
     this.mainService.getCategories(this.limit).subscribe({
-      next: (response) => {
-        this.categories = response.categories;
-      
-        if (this.categories.length > 0) {
-          this.onCategoryClick(this.categories[0], 0);
-        }
+      next: (categories: Category[]) => {
+        this.categories.set(categories);
       },
-      error: (error) => {
-      },
-    });
-  }
-
-  onCategoryClick(category: any, index: number): void {
-    this.selectedCategoryIndex = index;
-
-    this.mainService.getProductsByCategory(category.slug).subscribe({
-      next: (response) => {
-        // console.log('Products API Response:', response);
-        this.products = response.products; 
-      },
-      error: (error) => {
-        console.error('Error fetching products:', error);
-      },
+      error: (err: any) => {
+        console.error('Error fetching categories', err);
+      }
     });
   }
 }
