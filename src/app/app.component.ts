891f000f02c3098c87e5cf7ad4253ed3f3f9abfc
nohawaheed <nohawaheed.1991@gmail.com>
2025-01-22 18:00:10 +0200

    fix id collision error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  host: { hostID: crypto.randomUUID().toString() }, //added to fix component ID generation collision
 })
 export class AppComponent {
   title = 'Flower-Ecommerce';
