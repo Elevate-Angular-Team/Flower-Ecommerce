a7c57a07bd8cf1d2227a5b9555700328af07caae
nohawaheed <nohawaheed.1991@gmail.com>
2025-01-22 17:55:50 +0200

    change tailwind 2xl screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1440px",
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
